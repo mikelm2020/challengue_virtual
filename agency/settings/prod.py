7a1bf4ca9b2c3f0fa0218042c87017e924f5687c
Miguel Angel López <admon@tecnott.com.mx>
2022-12-18 23:51:15 -0600

    changes settings

--- agency/settings/prod.py
+++ agency/settings/prod.py
@@ -22,7 +22,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "simple_history.middleware.HistoryRequestMiddleware",
 ]
 
 DATABASES = {
